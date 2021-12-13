5700c1422ec46912b52a41eb634e2a83d296ce36
Xin Li <lixin@kylinos.cn>
2021-12-13 14:26:48 +0800

    Use theme pop window instead of frameless windows.

--- src/emptyNotes.cpp
+++ src/emptyNotes.cpp
@@ -32,8 +32,9 @@ emptyNotes::emptyNotes(QWidget *parent) :
 {
     ui->setupUi(this);
     setWindowTitle(tr("emptyNotes"));
-    this->setWindowFlags(Qt::FramelessWindowHint | Qt::Dialog);
+    setWindowFlags(Qt::Dialog);
     setAttribute(Qt::WA_TranslucentBackground);
+    setFixedSize(width(),height());
 
     ui->pushButton->setAutoRepeat(true);
     ui->pushButton->setAutoRepeatDelay(1);
@@ -42,12 +43,12 @@ emptyNotes::emptyNotes(QWidget *parent) :
     ui->pushButton_2->setAutoRepeatDelay(1);
     ui->pushButton_2->setAutoRepeatInterval(1);
 
-    // 添加窗管协议
-    MotifWmHints hints;
-    hints.flags = MWM_HINTS_FUNCTIONS|MWM_HINTS_DECORATIONS;
-    hints.functions = MWM_FUNC_ALL;
-    hints.decorations = MWM_DECOR_BORDER;
-    XAtomHelper::getInstance()->setWindowMotifHint(this->winId(), hints);
+//    // 添加窗管协议
+//    MotifWmHints hints;
+//    hints.flags = MWM_HINTS_FUNCTIONS|MWM_HINTS_DECORATIONS;
+//    hints.functions = MWM_FUNC_ALL;
+//    hints.decorations = MWM_DECOR_BORDER;
+//    XAtomHelper::getInstance()->setWindowMotifHint(this->winId(), hints);
 
 //    ui->widget->setStyleSheet("QWidget{background-color: rgba(255, 255, 255, 0.4);}");
 //    ui->label->setStyleSheet("background-color: rgba();\
