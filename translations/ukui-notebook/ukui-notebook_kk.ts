9c2255579aecba0e8781f2e78e11f63535df2655
oubayashi <214973957@qq.com>
2022-06-01 13:50:56 +0800

    feat(翻译): 添加维哈柯翻译文件

---
+++ translations/ukui-notebook/ukui-notebook_kk.ts
@@ -0,0 +1,365 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kk">
+<context>
+    <name>Edit_page</name>
+    <message>
+        <source>14</source>
+        <translation>14</translation>
+    </message>
+    <message>
+        <source>Form</source>
+        <translation>Пішін</translation>
+    </message>
+</context>
+<context>
+    <name>noteExitWindow</name>
+    <message>
+        <source>No</source>
+        <translation>Жоқ</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Иә</translation>
+    </message>
+    <message>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+    <message>
+        <source>Close the desktop note page at the same time</source>
+        <translation>Жұмыс үстелінің жазба бетін бір уақытта жабу</translation>
+    </message>
+    <message>
+        <source>Are you sure to exit the note book?</source>
+        <translation>Жазба кітабынан міндетті түрде шығасыз ба?</translation>
+    </message>
+</context>
+<context>
+    <name>Widget</name>
+    <message>
+        <source>New</source>
+        <translation>Жаңа</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Шығу</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Анықтама</translation>
+    </message>
+    <message>
+        <source>Menu</source>
+        <translation>Мәзір</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Шамамен</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Жабу</translation>
+    </message>
+    <message>
+        <source>Minimize</source>
+        <translation>Кішірейту</translation>
+    </message>
+    <message>
+        <source>Notes</source>
+        <translation>Жазбалар</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Іздеу</translation>
+    </message>
+    <message>
+        <source>Widget</source>
+        <translation>Виджет</translation>
+    </message>
+    <message>
+        <source>Create New Note</source>
+        <translation>Жаңа жазба жасау</translation>
+    </message>
+    <message>
+        <source>Switch View</source>
+        <translation>Көріністі ауыстыру</translation>
+    </message>
+    <message>
+        <source>Empty Note</source>
+        <translation>&amp; Ð Ð°Ð1/2</translation>
+    </message>
+    <message>
+        <source>Delete Selected Note</source>
+        <translation>Таңдалған жазбаны жою</translation>
+    </message>
+    <message>
+        <source>Welcome to use Notes.</source>
+        <translation>Жазбаларды пайдалануға қош келдіңіз.</translation>
+    </message>
+    <message>
+        <source>[picture]</source>
+        <translation>[сурет]</translation>
+    </message>
+</context>
+<context>
+    <name>EditPage</name>
+    <message>
+        <source>cut</source>
+        <translation>қиып алу</translation>
+    </message>
+    <message>
+        <source>Bold</source>
+        <translation>Қалың</translation>
+    </message>
+    <message>
+        <source>copy</source>
+        <translation>көшіру</translation>
+    </message>
+    <message>
+        <source>redo</source>
+        <translation>редо</translation>
+    </message>
+    <message>
+        <source>undo</source>
+        <translation>болдырмау</translation>
+    </message>
+    <message>
+        <source>Notes</source>
+        <translation>Жазбалар</translation>
+    </message>
+    <message>
+        <source>paste</source>
+        <translation>қою</translation>
+    </message>
+    <message>
+        <source>Select an image</source>
+        <translation>Кескінді таңдау</translation>
+    </message>
+    <message>
+        <source>copy to newpage</source>
+        <translation>жаңалыққа көшіру</translation>
+    </message>
+    <message>
+        <source>Italic</source>
+        <translation>Италич</translation>
+    </message>
+    <message>
+        <source>Font Size</source>
+        <translation>Қаріп өлшемі</translation>
+    </message>
+    <message>
+        <source>Font Color</source>
+        <translation>Қаріп түсі</translation>
+    </message>
+    <message>
+        <source>Unordered</source>
+        <translation>Unordered</translation>
+    </message>
+    <message>
+        <source>Ordered</source>
+        <translation>Тапсырысты</translation>
+    </message>
+    <message>
+        <source>InsertPicture</source>
+        <translation>КірістіруPicture</translation>
+    </message>
+    <message>
+        <source>Strikeout</source>
+        <translation>Соққы</translation>
+    </message>
+    <message>
+        <source>Underline</source>
+        <translation>Асты сызылсын</translation>
+    </message>
+    <message>
+        <source>JPEG (*.jpg);; GIF (*.gif);; PNG (*.png);; BMP (*.bmp);; All (*)</source>
+        <translation>JPEG (*.jpg); GIF (*.gif); PNG (*.png); BMP (*.bmp); Барлығы (*)</translation>
+    </message>
+</context>
+<context>
+    <name>emptyNotes</name>
+    <message>
+        <source>yes</source>
+        <translation>Иә</translation>
+    </message>
+    <message>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+    <message>
+        <source>No Tips</source>
+        <translation>Кеңестер жоқ</translation>
+    </message>
+    <message>
+        <source>cancel</source>
+        <translation>Болдырмау</translation>
+    </message>
+    <message>
+        <source>Are you sure empty notebook?</source>
+        <translation>Бос жазу кітапшасы бар ма?</translation>
+    </message>
+    <message>
+        <source>emptyNotes</source>
+        <translation>бос Ескертулер</translation>
+    </message>
+</context>
+<context>
+    <name>PaletteWidget</name>
+    <message>
+        <source>Form</source>
+        <translation>Пішін</translation>
+    </message>
+</context>
+<context>
+    <name>SelectColor</name>
+    <message>
+        <source>Form</source>
+        <translation>Пішін</translation>
+    </message>
+</context>
+<context>
+    <name>SetFontColor</name>
+    <message>
+        <source>Form</source>
+        <translation>Пішін</translation>
+    </message>
+</context>
+<context>
+    <name>SetFontSize</name>
+    <message>
+        <source>Form</source>
+        <translation>Пішін</translation>
+    </message>
+</context>
+<context>
+    <name>fontButton</name>
+    <message>
+        <source>Form</source>
+        <translation>Пішін</translation>
+    </message>
+</context>
+<context>
+    <name>noteHead</name>
+    <message>
+        <source>Form</source>
+        <translation>Пішін</translation>
+    </message>
+</context>
+<context>
+    <name>noteHeadMenu</name>
+    <message>
+        <source>Form</source>
+        <translation>Пішін</translation>
+    </message>
+    <message>
+        <source>Menu</source>
+        <translation>Мәзір</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Жабу</translation>
+    </message>
+    <message>
+        <source>Share</source>
+        <translation>Ортақ пайдалану</translation>
+    </message>
+    <message>
+        <source>Palette</source>
+        <translation>Палет</translation>
+    </message>
+    <message>
+        <source>Create New Note</source>
+        <translation>Жаңа жазба жасау</translation>
+    </message>
+    <message>
+        <source>Always in the front</source>
+        <translation>Әрқашан алдыңғы жағында</translation>
+    </message>
+    <message>
+        <source>Open note list</source>
+        <translation>Жазбалар тізімін ашу</translation>
+    </message>
+    <message>
+        <source>Delete this note</source>
+        <translation>Бұл жазбаны жою</translation>
+    </message>
+</context>
+<context>
+    <name>paletteButton</name>
+    <message>
+        <source>Form</source>
+        <translation>Пішін</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>show</source>
+        <translation>&amp; Көрсету</translation>
+    </message>
+</context>
+<context>
+    <name>About</name>
+    <message>
+        <source>Version: %1</source>
+        <translation>Нұсқасы:% 1</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Шамамен</translation>
+    </message>
+    <message>
+        <source>Notes</source>
+        <translation>Жазбалар</translation>
+    </message>
+    <message>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+    <message>
+        <source>Notes is a self-developed sidebar application plug-in, which provides a rich interface, convenient operation and stable functions, aiming at a friendly user experience.</source>
+        <translation>Ноталар - пайдаланушының ыңғайлы тәжірибесіне бағытталған бай интерфейсті, ыңғайлы жұмыс істеуді және тұрақты функцияларды қамтамасыз ететін өздігінен әзірленген бүйірлік қосымша қосылатын модулі.</translation>
+    </message>
+    <message>
+        <source>Service &amp; Support: </source>
+        <translation>Қызмет және қолдау: </translation>
+    </message>
+</context>
+<context>
+    <name>HeaderBar</name>
+    <message>
+        <source>Sort Type</source>
+        <translation>Сұрыптау түрі</translation>
+    </message>
+</context>
+<context>
+    <name>iconViewModeDelegate</name>
+    <message>
+        <source>Today  </source>
+        <translation>Бүгін  </translation>
+    </message>
+    <message>
+        <source>Yesterday  </source>
+        <translation>Кеше  </translation>
+    </message>
+</context>
+<context>
+    <name>listViewModeDelegate</name>
+    <message>
+        <source>Today  </source>
+        <translation>Бүгін  </translation>
+    </message>
+    <message>
+        <source>Yesterday  </source>
+        <translation>Кеше  </translation>
+    </message>
+</context>
+<context>
+    <name>NoteData</name>
+    <message>
+        <source>[picture]</source>
+        <translation>[сурет]</translation>
+    </message>
+</context>
+</TS>
