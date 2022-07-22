540fa545fb58db0b33bbfaa8bd0378dac1398665
oubayashi <214973957@qq.com>
2022-07-22 13:05:10 +0800

    fix(abouut): 关于界面使用sdk

--- src/about.h
+++ src/about.h
@@ -18,31 +18,15 @@
 #ifndef ABOUT_H
 #define ABOUT_H
 
-#include <QDialog>
-#include <QPainter>
-#include <QPainterPath>
-#include <QDesktopServices>
-#include <QUrl>
+#include <kysdk/applications/kaboutdialog.h>
 
-namespace Ui {
-class About;
-}
-
-class About : public QDialog
+class About : public kdk::KAboutDialog
 {
     Q_OBJECT
 
 public:
     explicit About(QWidget *parent = nullptr);
     ~About();
-
-protected:
-    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
-
-private:
-    Ui::About *ui;
-
-    void listenToGsettings();
 };
 
 #endif // ABOUT_H
